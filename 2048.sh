a671f7e5dcb3ff82fdd5e2e435abf5ecbdc394d9
kaqqy <james.shi@berkeley.edu>
2019-12-16 16:55:23 -0800

    added function for writing numbers to boxes

--- 2048.sh
+++ 2048.sh
@@ -9,6 +9,15 @@ SH=$(($BH * $GH + $GH + 1)) # new screen height
 OSW=$(tput cols) # old screen width
 OSH=$(tput lines) # old screen height
 
+# initialize board
+for i in $(seq 0 $(($GH - 1)))
+do
+    for j in $(seq 0 $(($GW - 1)))
+    do
+        board[$(($GW * $i + $j))]=""
+    done
+done
+
 # Repeatedly calls commands passed as parameters
 # Number of repetitions is the first parameter
 # Rest of the parameters are evaluated
@@ -30,6 +39,7 @@ repeat ()
     done
 }
 
+# Escapes \ and " in a string
 escape ()
 {
     if [[ $# -ne 1 ]]
@@ -40,9 +50,28 @@ escape ()
     echo $1 | sed 's/\(["\]\)/\\\1/g'
 }
 
+# Writes the number $3 to the box at row $1 and col $2
+writetobox ()
+{
+    if [[ $# -ne 3 ]]
+    then
+        echo "Error: Wrong number of arguments for writetobox()"
+        exit
+    elif [[ $1 -ge $GH ]] || [[ $1 -lt 0 ]] || [[ $2 -ge $GW ]] || [[ $2 -lt 0 ]]
+    then
+        echo "Error: Out of bounds coordinates for writetobox()"
+        exit
+    fi
+    local row=$(($1 * (1 + $BH) + 1 + $BH / 2))
+    local col=$(($2 * (1 + $BW) + 1 + ($BW - ${#3}) / 2))
+    tput cup $row $col
+    printf $3
+}
+
 tput smcup # show alternate screen
 printf "\e[8;${SH};${SW}t" # resize window
 tput clear
+# tput setab 14 # change background color
 
 # drawing the grid
 boxline="repeat $BW 'printf q'"
@@ -62,8 +91,15 @@ repeat $(($GW - 1)) "$boxline" "printf v"
 eval "$boxline"
 printf 'j'
 tput rmacs
-
+for i in {0..3}
+do
+    for j in {0..3}
+    do
+        writetobox $i $j $((4 * $i + $j))
+    done
+done
 sleep 5
+tput op
 tput rmcup # hide alternate screen
 printf "\e[8;${OSH};${OSW}t" # resize window
 exit
