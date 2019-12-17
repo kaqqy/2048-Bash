1ed932bfcda91a6b468b34518b72d0853b2247ee
kaqqy <james.shi@berkeley.edu>
2019-12-16 21:50:40 -0800

    added controls and basic visual updates

--- 2048.sh
+++ 2048.sh
@@ -5,7 +5,7 @@ GH=4 # height of grid
 BW=7 # width of grid box
 BH=3 # height of grid box
 SW=$(($BW * $GW + $GW + 1)) # new screen width
-SH=$(($BH * $GH + $GH + 1)) # new screen height
+SH=$(($BH * $GH + $GH + 2)) # new screen height
 OSW=$(tput cols) # old screen width
 OSH=$(tput lines) # old screen height
 
@@ -18,6 +18,14 @@ do
     done
 done
 
+# Writes an status log message to the screen ()
+status_log ()
+{
+    tput cup $(($SH - 1)) 0
+    tput el
+    printf "$@"
+}
+
 # Repeatedly calls commands passed as parameters
 # Number of repetitions is the first parameter
 # Rest of the parameters are evaluated
@@ -25,7 +33,7 @@ repeat ()
 {
     if [[ $# -lt 1 ]]
     then
-        echo "Error: Not enough arguments for repeat()"
+        status_log "Error: Not enough arguments for repeat()"
         exit
     fi
     count=$1
@@ -44,33 +52,86 @@ escape ()
 {
     if [[ $# -ne 1 ]]
     then
-        echo "Error: Need exactly one argument for escape()"
+        status_log "Error: Need exactly one argument for escape()"
         exit
     fi
     echo $1 | sed 's/\(["\]\)/\\\1/g'
 }
 
+# Shift the numbers in the grid in the specified direction
+shift_board ()
+{
+    if [[ $# -ne 1 ]]
+    then
+        status_log "Error: Wrong number of arguments for shift_board()"
+        exit
+    fi
+    # setup variables for handling all 4 directions at once
+    # up = 0, down = 1, left = 2, right = 3
+    local row_shift=$(((1 - $1 / 2) * (2 * $1 - 1)))
+    local col_shift=$((($1 / 2) * (2 * $1 - 5)))
+    # local ind_shift=$(((2 * ($1 % 2) - 1) * (1 + ($GW - 1) * (1 - $1 / 2))))
+    local range_start=$((($1 % 2) * ($GW * $GH - 1)))
+    local range_end=$(((1 - ($1 % 2)) * ($GW * $GH - 1)))
+    for i in $(seq $range_start $range_end)
+    do
+        local prev_row=$(($i / $GW))
+        local prev_col=$(($i % $GW))
+        local row=$(($prev_row + $row_shift))
+        local col=$(($prev_col + $col_shift))
+        while [[ $row -ge 0 ]] && [[ $row -lt $GH ]] && [[ $col -ge 0 ]] && [[ $col -lt $GW ]]
+        do
+            if [[ "${board[$(($GW * $row + $col))]}" != "" ]]
+            then
+                break
+            fi
+            board[$(($GW * $row + $col))]="${board[$(($GW * $prev_row + $prev_col))]}"
+            board[$(($GW * $prev_row + $prev_col))]=""
+            prev_row=$row
+            prev_col=$col
+            row=$(($row + $row_shift))
+            col=$(($col + $col_shift))
+        done
+    done
+}
+
 # Writes the number $3 to the box at row $1 and col $2
-writetobox ()
+write_to_box ()
 {
     if [[ $# -ne 3 ]]
     then
-        echo "Error: Wrong number of arguments for writetobox()"
+        status_log "Error: Wrong number of arguments for write_to_box()"
         exit
     elif [[ $1 -ge $GH ]] || [[ $1 -lt 0 ]] || [[ $2 -ge $GW ]] || [[ $2 -lt 0 ]]
     then
-        echo "Error: Out of bounds coordinates for writetobox()"
+        status_log "Error: Out of bounds coordinates for write_to_box()"
         exit
     fi
     local row=$(($1 * (1 + $BH) + 1 + $BH / 2))
-    local col=$(($2 * (1 + $BW) + 1 + ($BW - ${#3}) / 2))
+    local col=$(($2 * (1 + $BW) + 1))
     tput cup $row $col
-    printf $3
+    printf "%${BW}s"
+    tput cup $row $(($col + ($BW - ${#3}) / 2))
+    printf "$3"
+}
+
+# Updates all numbers on the board
+update_board()
+{
+    for i in $(seq 0 $(($GH - 1)))
+    do
+        for j in $(seq 0 $(($GW - 1)))
+        do
+            write_to_box $i $j "${board[$(($GW * $i + $j))]}"
+        done
+    done
 }
 
 tput smcup # show alternate screen
 printf "\e[8;${SH};${SW}t" # resize window
-tput clear
+tput clear # clears screen
+tput civis # hides cursor
+stty -echo # hides keyboard input
 # tput setab 14 # change background color
 
 # drawing the grid
@@ -91,34 +152,58 @@ repeat $(($GW - 1)) "$boxline" "printf v"
 eval "$boxline"
 printf 'j'
 tput rmacs
-for i in {0..3}
+
+for i in {1..19}
 do
-    for j in {0..3}
-    do
-        writetobox $i $j $((4 * $i + $j))
-    done
+    board[$(($RANDOM % ($GW * $GH)))]=$RANDOM
 done
-sleep 5
-tput op
+update_board
+
+str="   "
+read -n 1 -s char
+while [[ $char != 'q' ]]
+do
+    str="${str:1}$char"
+    update=1
+    case $str in
+        *w | $'\e[A')
+            status_log up
+            shift_board 0
+            shift_board 0
+            ;;
+        *s | $'\e[B')
+            status_log down
+            shift_board 1
+            shift_board 1
+            ;;
+        *d | $'\e[C')
+            status_log right
+            shift_board 3
+            shift_board 3
+            ;;
+        *a | $'\e[D')
+            status_log left
+            shift_board 2
+            shift_board 2
+            ;;
+        *)
+            update=0
+            ;;
+    esac
+    if [[ $update -eq 1 ]]
+    then
+        update_board
+        # add new random block (2 or 4)
+        # check if game is over (all filled)
+    fi
+    read -n 1 -s char
+done
+
+stty echo # shows keyboard input
+tput op # reset any color/style changes
+tput cnorm # unhides cursor
 tput rmcup # hide alternate screen
 printf "\e[8;${OSH};${OSW}t" # resize window
 exit
 
-#tput cup 10 10
-
-exit
-
-read -n 1 -s char
-while [[ $char != 'q' ]]
-do
-    echo $char
-    read -n 1 -s char
-done
-tput rmcup
-exit
 printf '\a'
-sleep 5
-printf '\e[2J'
-#printf '\e[90A'
-printf 'asdfasdf\n'
-sleep 5
